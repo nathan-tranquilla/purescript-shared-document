7bf13f87ec40476916952fb769ef0b4a87d1a5c4
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-08-05 10:50:04 -0300

    add linter

--- src/App.purs
+++ src/App.purs
@@ -20,11 +20,9 @@ import Web.HTML.HTMLTextAreaElement (selectionStart, fromEventTarget, value)
 import Web.UIEvent.KeyboardEvent (KeyboardEvent, key, toEvent)
 import WebCrypto (computeSHA256)
 
-type State 
-  = { input :: Maybe String, pos :: Maybe Int, version :: Int }
+type State = { input :: Maybe String, pos :: Maybe Int, version :: Int }
 
-data Action
-  = Input String (Maybe EventTarget)
+data Action = Input String (Maybe EventTarget)
 
 component :: forall q i o m. MonadEffect m => H.Component q i o m
 component =
@@ -40,7 +38,7 @@ render state =
     [ HH.textarea
         [ HE.onKeyDown \event -> Input (key event) (target $ toEvent event)
         ]
-      , HH.div_ [HH.text (showState state) ]
+    , HH.div_ [ HH.text (showState state) ]
     ]
 
 handleAction :: forall cs o m. MonadEffect m => Action -> H.HalogenM State Action cs o m Unit
@@ -49,23 +47,27 @@ handleAction = case _ of
     H.liftEffect $ log $ "Key pressed: " <> k -- Log the key pressed
     case maybeTarget >>= fromEventTarget of
       Just el -> do
-        pos       <- H.liftEffect $ selectionStart el
+        pos <- H.liftEffect $ selectionStart el
         H.liftEffect $ logShow $ "Cursor position: " <> show pos -- Log the cursor position
         H.modify_ \st -> st { input = Just k, pos = Just pos, version = st.version + 1 }
       Nothing -> do
         H.liftEffect $ log "No valid target found." -- Log if no valid target is found
         pure unit
-  
-showState :: State -> String 
-showState state = foldl (\acc txt -> acc <> " " <> txt) "" 
-  $ filter (\str -> length str > 0) [showText "Input" state.input,showPos state.pos, "Version: " <> show state.version]
 
-showText :: String -> Maybe String -> String 
-showText txt mInput = case mInput of 
+showState :: State -> String
+showState state = foldl (\acc txt -> acc <> " " <> txt) ""
+  $ filter (\str -> length str > 0)
+      [ showText "Input" state.input
+      , showPos state.pos
+      , "Version: " <> show state.version
+      ]
+
+showText :: String -> Maybe String -> String
+showText txt mInput = case mInput of
   Just inpt -> txt <> ": " <> inpt
-  Nothing   -> ""
+  Nothing -> ""
 
-showPos :: Maybe Int -> String 
-showPos mInt = case mInt of 
-  Just int  -> "Position: " <> (show int)
-  Nothing   -> ""
+showPos :: Maybe Int -> String
+showPos mInt = case mInt of
+  Just int -> "Position: " <> (show int)
+  Nothing -> ""
